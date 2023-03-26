632e91aadb4663e8932dec8c2ed4218949c5baf0
go140point6 <ryan@go140point6.com>
2023-03-26 12:05:16 -0700

    remove per USD on pool

--- commands/pool.js
+++ commands/pool.js
@@ -65,7 +65,8 @@ module.exports = {
 				.addFields(
 					{ name: 'Pool Value (wFLR):', value: `${hrWFLR}`},
 					{ name: 'Pool USD Value:', value: `${flrPoolUSD}`},
-					{ name: 'Pool Share per Mingo:', value: `${flrShare} wFLR (${usdShare})`},
+					//{ name: 'Pool Share per Mingo:', value: `${flrShare} wFLR (${usdShare})`},
+					{ name: 'Pool Share per Mingo:', value: `${flrShare} wFLR`},
 				)
 				//.setImage('https://onxrp-marketplace.s3.us-east-2.amazonaws.com/nft-images/00081AF4B6C6354AE81B765895498071D5E681DB44D3DE8F1589271700000598-32c83d6e902f8.png')
 				.setTimestamp()
