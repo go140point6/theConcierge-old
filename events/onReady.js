820c7d078a71218a2903d69411a8f73cc4778321
go140point6 <ryan@go140point6.com>
2023-03-31 20:38:59 -0700

    epoch lock period alarm

--- events/onReady.js
+++ events/onReady.js
@@ -6,10 +6,51 @@ const path = require('node:path');
 const client = require('../index');
 const { REST, Routes, Collection, EmbedBuilder } = require('discord.js');
 const axios = require('axios');
+const ethers = require('ethers')
 const Database = require('better-sqlite3');
 const chalk = require('chalk'); // npm install chalk@v4.1.2
 const decrease = chalk.bold.red
 const increase = chalk.bold.green
+
+const fCR = "abi/flareContractRegistry.abi"
+const fFR = "abi/flareFtsoRegistry.abi"
+const fFM = "abi/flareFtsoManager.abi"
+const fCRabi = JSON.parse(fs.readFileSync(fCR))
+const fFRabi = JSON.parse(fs.readFileSync(fFR))
+const fFMabi = JSON.parse(fs.readFileSync(fFM))
+
+var ftsoRegistryCA
+var ftsoManagerCA
+var flrPrice
+var flrCurrentEpoch
+var flrCurrentEpochEnds
+var flrCurrentLockzoneBegins
+var flrLockCurrentDeadlineMS
+var flrLockAlarm1
+var flrLockAlarm2
+var flrLockAlarm3
+var flrLockAlarm1Set = 0
+var flrLockAlarm2Set = 0
+var flrLockAlarm3Set = 0
+var id  = '839645617911234601'
+
+var provider = new ethers.JsonRpcProvider(
+    "https://flare-api.flare.network/ext/C/rpc"
+    )
+
+// Flare Contract Regiatry address
+// the only address that should be hard-coded
+const flareContractRegistry = {
+    address: "0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019",
+    abi: fCRabi,
+}
+
+// Create Flare Contract Registry instance
+const flareContractRegistryInstance = new ethers.Contract(
+    flareContractRegistry.address,
+    flareContractRegistry.abi,
+    provider
+)
 
 const formatterDecimal = new Intl.NumberFormat('en-US', {
 	style: 'decimal',
@@ -41,6 +82,7 @@ function onReady(client) {
     setInterval(async function() {
         await doMintCalls()
         await doAPICalls()
+        await doEpochCalls()
         console.log("currentWFLR: ", currentWFLR)
         console.log("prevWFLR: ", prevWFLR)
         if ( prevWFLR === 0 ) {
@@ -141,7 +183,84 @@ function onReady(client) {
                 //channel.send('Hello there!');
                 }
                 count = 0
-            }    
+            }
+            
+            let checkAlarm = Date.now()
+            console.log("Current time (miliseconds): ", checkAlarm)
+            if (flrLockAlarm1Set == 0) {
+                console.log("Alarm 1 is 0")
+                if (checkAlarm > flrLockAlarm1) {
+                    console.log("Current time is less than Alarm 1 time, so trigger embed")
+                    flrLockAlarm1Set = 1
+
+                    const embedAlarm1 = new EmbedBuilder()
+                    .setColor('LuminousVividPink')
+                    .setTitle(`Flavio here, need anything else?`)
+                    //.setAuthor({ name: client.user.username })
+                    .setDescription(`Flare Epoch Update:`)
+                    .setThumbnail(client.user.avatarURL())
+                    .addFields(
+                        { name: 'Lock Period for next epoch starts in: ', value: `12 Hours` },
+                        { name: 'Lock Period start date/time:', value: `${flrCurrentLockzoneBegins} PDT` },
+                        { name: 'Lock Period and Epoch ends:', value: `${flrCurrentEpochEnds} PDT` },
+                    )
+                    //.setImage('https://media.tenor.com/Egt2H3v94ZYAAAAd/dog-pool.gif')
+                    .setTimestamp()
+                    //.setFooter({ text: 'Powered by CoinGecko', iconURL: 'https://images2.imgbox.com/5f/85/MaZQ6yi0_o.png' });
+
+                channel_mint.send({ content:`<@${id}>`, embeds: [embedAlarm1] })
+                }
+            }
+
+            if (flrLockAlarm2Set == 0) {
+                console.log("Alarm 2 is 0")
+                if (checkAlarm > flrLockAlarm2) {
+                    console.log("Current time is less than Alarm 2 time, so trigger embed")
+                    flrLockAlarm2Set = 1
+
+                    const embedAlarm2 = new EmbedBuilder()
+                    .setColor('LuminousVividPink')
+                    .setTitle(`Flavio here, need anything else?`)
+                    //.setAuthor({ name: client.user.username })
+                    .setDescription(`Flare Epoch Update:`)
+                    .setThumbnail(client.user.avatarURL())
+                    .addFields(
+                        { name: 'Lock Period for next epoch starts in: ', value: `8 Hours` },
+                        { name: 'Lock Period start date/time:', value: `${flrCurrentLockzoneBegins} PDT` },
+                        { name: 'Lock Period and Epoch ends:', value: `${flrCurrentEpochEnds} PDT` },
+                    )
+                    //.setImage('https://media.tenor.com/Egt2H3v94ZYAAAAd/dog-pool.gif')
+                    .setTimestamp()
+                    //.setFooter({ text: 'Powered by CoinGecko', iconURL: 'https://images2.imgbox.com/5f/85/MaZQ6yi0_o.png' });
+
+                channel_mint.send({ content:`<@${id}>`, embeds: [embedAlarm2] })
+                }
+            }
+
+            if (flrLockAlarm3Set == 0) {
+                console.log("Alarm 3 is 0")
+                if (checkAlarm > flrLockAlarm3) {
+                    console.log("Current time is less than Alarm 3 time, so trigger embed")
+                    flrLockAlarm3Set = 1
+
+                    const embedAlarm3 = new EmbedBuilder()
+                    .setColor('LuminousVividPink')
+                    .setTitle(`Flavio here, need anything else?`)
+                    //.setAuthor({ name: client.user.username })
+                    .setDescription(`Flare Epoch Update:`)
+                    .setThumbnail(client.user.avatarURL())
+                    .addFields(
+                        { name: 'Lock Period for next epoch starts in: ', value: `4 Hours` },
+                        { name: 'Lock Period start date/time:', value: `${flrCurrentLockzoneBegins} PDT` },
+                        { name: 'Lock Period and Epoch ends:', value: `${flrCurrentEpochEnds} PDT` },
+                    )
+                    //.setImage('https://media.tenor.com/Egt2H3v94ZYAAAAd/dog-pool.gif')
+                    .setTimestamp()
+                    //.setFooter({ text: 'Powered by CoinGecko', iconURL: 'https://images2.imgbox.com/5f/85/MaZQ6yi0_o.png' });
+
+                channel_mint.send({ content:`<@${id}>`, embeds: [embedAlarm3] })
+                }
+            }
     }, process.env.INTERVAL_POOL);
         
     client.commands = new Collection();
@@ -191,6 +310,7 @@ function onReady(client) {
     //setInterval(getFLRToken, Math.max(1, 5 || 1) * 60 * 1000);
     doAPICalls();
     doMintCalls();
+    doEpochCalls();
     // the functions above setIntervalMint and Pool, are self running based on that interval
     //setInterval(doAPICalls, Math.max(1, 5 || 1) * 60 * 1 * 1000);
     //setInterval(doMintCalls, Math.max(1, 10 || 1) * 60 * 1 * 1000);
@@ -198,6 +318,57 @@ function onReady(client) {
     //setInterval(doCheckPool, Math.max(1, 3 || 1) * 60 * 1 * 1000);
     
 };
+
+async function getFlrEpochInfo() {
+    ftsoManagerCA = await flareContractRegistryInstance.getContractAddressByName('FtsoManager')
+    //console.log("FTSO Manager CA: ", ftsoManagerCA)
+
+    const flareFtsoManager = {
+        address: ftsoManagerCA,
+        abi: fFMabi,
+    }
+
+    const flareFtsoManagerInstance = new ethers.Contract(
+        flareFtsoManager.address,
+        flareFtsoManager.abi,
+        provider
+    )
+
+    //console.log(flareFtsoManagerInstance)
+    let flrEpoch = await flareFtsoManagerInstance.getCurrentRewardEpoch()
+    let flrEpochEnds = await flareFtsoManagerInstance.currentRewardEpochEnds()
+    let flrEpochDuration = await flareFtsoManagerInstance.rewardEpochDurationSeconds()
+    //console.log(typeof(flrEpoch))
+    flrCurrentEpoch = Number(flrEpoch)
+    //console.log("Current Flare Epoch ", Number(flrEpoch))
+    //console.log("Reward Epoch to Expire Next: ", Number(flrExpireEpoch))
+
+    let flrEpochEndsMS = Number(flrEpochEnds)*1000
+    //console.log("Current Reward Epoch Ends (miliseconds): ", flrEpochEndsMS)
+    let hrFlrEpochEnds = new Date(flrEpochEndsMS)
+    //console.log("Current Reward Epoch Ends (UTC): ", hrFlrEpochEnds.toUTCString())
+    //console.log("Current Reward Epoch Ends (Local): ", hrFlrEpochEnds.toLocaleString())
+    flrCurrentEpochEnds = hrFlrEpochEnds.toLocaleString()
+    
+    let flrEpochDurationMS = Number(flrEpochDuration)*1000
+    //console.log("Epoch Duration (miliseconds): ", flrEpochDurationMS)
+    let flrLockDeadlineMS = flrEpochDurationMS/2
+    //console.log("Epoch Lock Deadline (miliseconds): ", flrLockDeadlineMS)
+
+    flrLockCurrentDeadlineMS = flrEpochEndsMS - flrLockDeadlineMS
+    flrLockAlarm1 = flrLockCurrentDeadlineMS - 43200000
+    flrLockAlarm2 = flrLockCurrentDeadlineMS - 28800000
+    flrLockAlarm3 = flrLockCurrentDeadlineMS - 14400000
+    console.log("Epoch Lock Current Deadline (miliseconds): ", flrLockCurrentDeadlineMS)
+    console.log("Epoch Lock Alarm 1 (miliseconds): ", flrLockAlarm1)
+    console.log("Epoch Lock Alarm 2 (miliseconds): ", flrLockAlarm2)
+    console.log("Epoch Lock Alarm 3 (miliseconds): ", flrLockAlarm3)
+    
+    let hrFlrLockDeadline = new Date(flrLockCurrentDeadlineMS)
+    //console.log("Current Lock Deadline (UTC): ", hrFlrLockDeadline.toUTCString())
+    //console.log("Current Lock Deadline (Local): ", hrFlrLockDeadline.toLocaleString())
+    flrCurrentLockzoneBegins = hrFlrLockDeadline.toLocaleString()
+}
 
 async function getFLR() {
     //await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=flare-networks`).then(res => {
@@ -274,6 +445,10 @@ async function sendMessage(client) {
     }
 }
 */
+
+async function doEpochCalls() {
+    await getFlrEpochInfo();
+}
 
 async function doMintCalls() {
     await getMint();
